bd9fdec973d6d0f76a23176859ee89d01548c478
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-11 15:02:42 +0530

    first we should have to import qr module to generate , Create simple QR code generator using user-provided text and filename

--- QR_code.py
+++ QR_code.py
@@ -1,4 +1,9 @@
 #just making simple qr code 
+# import qrcode as qr
+# img =qr.make("www.linkedin.com/in/m-mahammad-khaleed-6a8a30313")
+# img.save("Linkedin_profile.png")
+
 import qrcode as qr
-img =qr.make("www.linkedin.com/in/m-mahammad-khaleed-6a8a30313")
-img.save("Linkedin_profile.png")
+
+data =input("Enter the Text or URL: ").strip()
+filename -input("Enter the fil")
