00f995699bf87b495ca52091e06a43a899111a33
KHALEED\mulla <mullamohammadk123@gmail.com>
2025-04-11 15:16:26 +0530

    Customize QR code colors and save with user-defined filename

--- QR_code.py
+++ QR_code.py
@@ -3,12 +3,16 @@
 # img =qr.make("www.linkedin.com/in/m-mahammad-khaleed-6a8a30313")
 # img.save("Linkedin_profile.png")
 
-import qrcode
+import qrcode 
  
-from PIL import Image
 data =input("Enter the Text or URL: ").strip()
-filename -input("Enter the filename:").strip()
+filename=input("Enter the filename:").strip()
 
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
                    box_size=20,border=10)
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill_color="red",back_color="blue")
+img.save(filename)
+print(f'QR code saved as {filename}')
